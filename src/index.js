05c4fd43725f7c9c66d798ada2e3b1395fda486e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 21:50:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -68,25 +68,5 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //add event listener to create a new toy button to call submitNewToy(name, imageUrl)
-document.querySelector(".submit").addEventListener("click", function (event) {
-  function submitNewToy(name, image, likes=0) {
-    return fetch("http://localhost:3000/toys", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        "name": name,
-        "image": image,
-        "likes" : 0
-      })
-    })
-    .then(function(response) {
-      return response.json();
-    })
-      .then(function(object) {
-        createToyCard(name, image, likes)
-      })
-    }
-})
+  document.querySelector(".submit").addEventListener("click", function (event) {
+  console.log("clicked!") })
