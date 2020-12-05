a83a7d2dca4d681a790a1f7689534b0c256f8ee6
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 21:43:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -90,4 +90,6 @@ function submitNewToy(name, image, likes=0) {
   }
 
   //add event listener to create a new toy button to call submitNewToy(name, imageUrl)
-document.querySelector(".submit").addEventListener("click", submitNewToy(name, image, likes= 0));
+document.querySelector(".submit").addEventListener("click", function (event) {
+  submitNewToy(name, image, likes= 0));
+}
